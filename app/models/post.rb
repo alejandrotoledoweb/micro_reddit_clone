5effb98ce3f05fbdb7f63925f761253c777150b2
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2020-11-13 13:55:32 -0800

    add associations part

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+    belongs_to :user
+    validates :title, confirmation: true, presence: true
+    validates :article, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: 'only allows letters' }
+    validates :email, confirmation: true, presence: true
+    validates :nickname, presence: true
+
+    has_many :posts
+end
