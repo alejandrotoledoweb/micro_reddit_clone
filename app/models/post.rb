0aef3fc608a94d87618ce3cfc447ae98b55f99d1
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2020-11-13 14:27:09 -0800

    fix linterserrors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    validates :title, confirmation: true, presence: true
-    validates :article, presence: true
+  belongs_to :user
+  validates :title, confirmation: true, presence: true
+  validates :article, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
-    message: 'only allows letters' }
-    validates :email, confirmation: true, presence: true
-    validates :nickname, presence: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+                                             message: 'only allows letters' }
+  validates :email, confirmation: true, presence: true
+  validates :nickname, presence: true
 
-    has_many :posts
+  has_many :posts
 end
