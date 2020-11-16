f1bf4aa3dc7d59e5143924c9b9e7f62acffbe8d7
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-15 21:18:14 -0500

    add has many comments and uniqueness

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, confirmation: true, presence: true
   validates :article, presence: true
+
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
                                              message: 'only allows letters' }
-  validates :email, confirmation: true, presence: true
-  validates :nickname, presence: true
+  validates :email, confirmation: true, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true
 
   has_many :posts
   has_many :comments
