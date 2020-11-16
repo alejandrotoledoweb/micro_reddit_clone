d3d612160df77baf6db4f5ce4fafa84e62a321e1
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-15 21:35:12 -0500

    git add uniqueness and length missing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, confirmation: true, presence: true
+  validates :title, uniqueness: true, presence: true
   validates :article, presence: true
 
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
                                              message: 'only allows letters' }
   validates :email, confirmation: true, presence: true, uniqueness: true
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true, length: { in: 6..16 }
 
   has_many :posts
   has_many :comments
